985c2be914982902e1894e439fa73705b9b6ebae
Devika-Baddani <75314277+Devika-Baddani@users.noreply.github.com>
2020-12-26 18:23:55 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+from flask import Flask,render_template,request,redirect,url_for,flash,abort
 import sqlite3 as sql
 from datetime import datetime,timedelta
 import time as t
@@ -22,14 +22,23 @@ def shortbell():
     t.sleep(1)'''
 app=Flask(__name__)
 app.secret_key = 'PBSAasdertyuiop2020'
+user_pass = {0:"user",1:"password"}
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title = '404'), 404
 @app.route('/')
 def login():
+    user_pass[0]="user"
+    user_pass[1]="password"
     return render_template('login.html')
 @app.route('/loginvalidation',methods = ["POST"])
 def loginvalid():
     username = request.form['username']
     password = request.form['password']
     if username=="admin" and password=="asdf":
+        user_pass[0] = username
+        user_pass[1] = password
         return render_template('selection2.html')
     else:
         return render_template('login.html')
@@ -42,19 +51,19 @@ def send():
 di = {0:"decision"}
 #de = {0:"normaldays",1:"internal",2:"endsem",3:"stop"}
 def total(act,start,end):
-    print(di)
     a = datetime.strptime(start,"%Y-%m-%d")
     b = datetime.strptime(end,"%Y-%m-%d")
     delta = b-a
-    print("Total number of days: ",delta.days)
     print(act)
+    print("Total number of days: ",delta.days)
+    print("Scheduled for the following days: ")
     di[0] = act
-    print(di)
     dates = {}
     for i in range(0,delta.days):
         day = a + timedelta(days=i)
         dates[str(day)[0:10]] = i+1
-    print("Total dates dictionary:",dates)
+        print(str(day)[0:10])
+    #print("Total dates dictionary:",dates)
     if(act == "normaldays"):
         s = {"08:00:00":2,"08:50:00":3,"09:40:00":4,"10:30:00":5,"10:45:00":6,"11:35:00":7,"12:25:00":8,"13:15:00":9,"14:05:00":10,"14:55:00":11,"15:10:00":12,"16:00:00":13,"16:50:00":14,"17:40:00":15}
         c = {"08:00:00":2,"08:50:00":1,"09:40:00":2,"10:30:00":3,"10:45:00":1,"11:35:00":4,"12:25:00":5,"13:15:00":1,"14:05:00":2,"14:55:00":3,"15:10:00":1,"16:00:00":4,"16:50:00":5,"17:40:00":6}
@@ -85,21 +94,18 @@ def total(act,start,end):
             date = t.strftime("%Y-%m-%d")
             m = t.strftime("%H:%M:%S")
             if m in l and date in dates:
-                print(m)
                 p = 1 if l.get(m) is None else l.get(m)
-                print("p = ",p)
+                #print("p = ",p)
                 q = len(dates) if dates.get(date) is None else dates.get(date)
-                print("q = ",q)
+                #print("q = ",q)
                 print("long bell")
-                #thr = Thread(target = send)
-                #thr.start()
                 #longbell()
                 t.sleep(1)
             elif m in s and date in dates:
                 p = 1 if s.get(m) is None else s.get(m)
-                print("p = ", p)
+                #print("p = ", p)
                 q = len(dates) if dates.get(date) is None else dates.get(date)
-                print("q = ", q)
+                #print("q = ", q)
                 r = c.get(m)
                 print("The bell should ring", r," times")
                 #thr = Thread(target = send)
@@ -108,115 +114,108 @@ def total(act,start,end):
                     print("bell rings")
                     #shortbell()
                     t.sleep(1)
-        print("previous schedule exits 1st while loop")
-    print("previous schedule exits 2nd while loop")
 
-
-def new(d_s,d_l,c):
-    p = 0
+def new(start,end,d_s,d_l,c,act):
+    dates = {}
+    a = datetime.strptime(start,"%Y-%m-%d")
+    b = datetime.strptime(end,"%Y-%m-%d")
+    delta = b-a
+    print(act)
+    print("Total number of days: ",delta.days)
+    print("The following dates are scheduled: ")
+    di[0] = act
+    #print(di)
+    dates = {}
+    for i in range(0,delta.days):
+        day = a + timedelta(days=i)
+        dates[str(day)[0:10]] = i+1
+        print(str(day)[0:10])
+    #print("Total dates dictionary:",dates)
+    q = 0
     s_k = list(d_s.keys())
-    while p < len(d_s)+len(d_l):
-        date = t.strftime("%Y-%m-%d")
-        m = t.strftime("%H:%M:%S")
-        if m in d_l:
-            print(m)
-            print(date)
-            p = 1 if d_l.get(m) is None else d_l.get(m)
-            print("p = ",p)
-            print("long bell")
-            #thr = Thread(target = send)
-            #thr.start()
-            #longbell()
-            t.sleep(1)
-        elif m in d_s:
-            print(m)
-            print(date)
-            p = 1 if d_s.get(m) is None else d_s.get(m)
-            print("p = ",p)
-            r = c.get(m)
-            print("The bell should ring", r," times")
-            #thr = Thread(target = send)
-            #thr.start()
-            for i in range(r):
-                print("bell rings")
-                #shortbell()
+    while q< len(dates) and act== di.get(0):
+        p = 0 
+        while p < len(d_s)+len(d_l) and act==di.get(0):
+            date = t.strftime("%Y-%m-%d")
+            m = t.strftime("%H:%M:%S")
+            if m in d_l and date in dates:
+                print(m)
+                p = 1 if d_l.get(m) is None else d_l.get(m)
+                #print("p = ",p)
+                q = len(dates) if dates.get(date) is None else dates.get(date)
+                #print("q = ",q)
+                print("long bell")
+                #longbell()
                 t.sleep(1)
-    print("while loop exits")
+            elif m in d_s and date in dates:
+                print(m)
+                p = 1 if d_s.get(m) is None else d_s.get(m)
+                #print("p = ",p)
+                q = len(dates) if dates.get(date) is None else dates.get(date)
+                #print("q = ",q)
+                r = c.get(m)
+                print("The bell should ring", r," times")
+                for i in range(r):
+                    print("bell rings")
+                    #shortbell()
+                    t.sleep(1)
 
 def EMERGENCY():
     print("longbell")
-    longbell()
+    #longbell()
     date = t.strftime("%Y-%m-%d")
     a = datetime.strptime(date,"%Y-%m-%d")
     b = a + timedelta(days=1)
     s_d = str(a)[0:10]
-    print(s_d)
     e_d = str(b)[0:10]
-    print(e_d)
-    #all("stay",s_d,e_d)
     thr = Thread(target = total, args=["stop",s_d,e_d])
     thr.start()
 
 @app.route('/classify',methods = ["GET","POST"])
 def classify():
-    action = request.form['regular']
-    #print(action)
-    if action == 'regular':
+    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+        action = request.form['regular']
+        if action == 'regular':
             return render_template('selection1.html')
-    #elif action == 'examination':
-    #        return render_template('selection page.html')
-    elif action == 'emergency':
+        elif action == 'emergency':
             thr = Thread(target = EMERGENCY)
             thr.start()
-            return render_template('login.html')
-    elif action == 'create':
+            return redirect(url_for('login'))
+        elif action == 'create':
             return redirect(url_for('create'))
-    #elif action == 'stop':
-    #        return render_template('selection2.html')
+        elif action == 'logout':
+            return redirect(url_for('login'))
+        return render_template('selction2.html')
+    else:
+        abort(404)
 @app.route('/exam',methods=["POST","GET"])
 def exam():
-    a = request.form
-    print(a)
-    con = sql.connect("real1.db") 
-    cur = con.cursor()
-    cursor = con.execute("SELECT name from sqlite_master WHERE type = 'table' AND name = 'DATE';")
-    result = cursor.fetchone()
-    cur.execute("DROP TABLE IF EXISTS DATA")
-    print("table droped successfully")
-    cur.execute('''CREATE TABLE IF NOT EXISTS DATA(SNO INTEGER PRIMARY KEY AUTOINCREMENT,DAY TEXT NOT NULL,STARTDATE TEXT NOT NULL,ENDDATE TEXT NOT NULL)''')
-    cur.execute("INSERT INTO DATA(DAY,STARTDATE,ENDDATE) VALUES (?,?,?)",(str(a['internal']),str(a['startDate']),str(a['endDate']),))
-    con.commit()
-    #con.close()
-    print("database saved")
-    flash("Successfully Saved")
-    cursor =con.execute("select DAY,STARTDATE,ENDDATE from DATA")
-    action = request.form['internal']
-    for row in cursor:
-        print(row[0])
-        print(row[1])
-        print(row[2])
-        act = row[0]
-        s_d = row[1]
-        e_d = row[2]
-    action = request.form['internal']
-    #all(act,s_d,e_d)
-    di[0] = "decision"
-    thr = Thread(target = total, args = [act,s_d,e_d])
-    thr.start()
-    return render_template('selection1.html')
-    '''if act=="internal" and action==row[0]:
-        inter(row[1],row[2])
+    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+        a = request.form
+        con = sql.connect("real1.db") 
+        cur = con.cursor()
+        cursor = con.execute("SELECT name from sqlite_master WHERE type = 'table' AND name = 'DATE';")
+        result = cursor.fetchone()
+        cur.execute("DROP TABLE IF EXISTS DATA")
+        #print("table droped successfully")
+        cur.execute('''CREATE TABLE IF NOT EXISTS DATA(SNO INTEGER PRIMARY KEY AUTOINCREMENT,DAY TEXT NOT NULL,STARTDATE TEXT NOT NULL,ENDDATE TEXT NOT NULL)''')
+        cur.execute("INSERT INTO DATA(DAY,STARTDATE,ENDDATE) VALUES (?,?,?)",(str(a['internal']),str(a['startDate']),str(a['endDate']),))
+        con.commit()
+        #con.close()
+        #print("database saved")
+        flash("Successfully Saved")
+        cursor =con.execute("select DAY,STARTDATE,ENDDATE from DATA")
+        action = request.form['internal']
+        for row in cursor:
+            act = row[0]
+            s_d = row[1]
+            e_d = row[2]
+        di[0] = "decision"
+        thr = Thread(target = total, args = [act,s_d,e_d])
+        thr.start()
         return render_template('selection1.html')
-    elif act=="semester" and action == row[0]:
-        endsem(row[1],row[2])
-        return render_template('selection1.html')
-    elif act=="normaldays" and action == row[0]:
-        REGULAR(row[1],row[2])
-        return render_template('selection1.html')
-    elif act== 'stop' and action == row[0]:
-        stay(row[1],row[2])
-        return render_template('selection1.html')
-    return render_template('selection1.html')'''
+    else:
+        abort(404)
 
 
 @app.route('/create',methods=["GET","POST"])
@@ -224,87 +223,96 @@ def create():
     time = []
     text = []
     bell = []
-    if request.method=="POST":
-        if request.form.get("submit"):
-            print(len(request.form))
-            data = request.form.to_dict(flat=False)
-            total = len(data)+1
-            t = int(total/3)
-            for i in range(0,t):
-                ti=data['time'+str(i)]
-                te=data['text'+str(i)]
-                be=data['select'+str(i)]
-                time.append(ti)
-                text.append(te)
-                bell.append(be)
-            con = sql.connect("real.db") 
-            cur = con.cursor()
-            cur.execute('''CREATE TABLE IF NOT EXISTS SchoolBells(SNO INTEGER PRIMARY KEY AUTOINCREMENT,TIME TEXT NOT NULL,BELLS TEXT NOT NULL,COUNT INT NOT NULL)''')
-            for i in range(0,len(time)):
-                cur.execute("INSERT INTO SchoolBells(TIME,COUNT,BELLS) VALUES (?,?,?)",(str(time[i][0]),str(text[i][0]),str(bell[i][0]),))
-            con.commit()
-            con.close()
-            print("database saved")
-            flash("Successfully Saved")
-            print(time)
-            print(text)
-            print(bell)
-            d_s = {}
-            d_l = {}
-            c = {}
-            for i in range(0,t):
-                if bell[i][0] == 'short bell':
-                    d_s[time[i][0]] = i+1
-                    c[time[i][0]]=int(text[i][0])
-                if bell[i][0] == 'long bell':
-                    d_l[time[i][0]] = i+1
-            print(d_s)
-            print(d_l)
-            print(c)
-            #new(d_s,d_l,c)
-            thr = Thread(target= new, args=[d_s,d_l,c])
-            thr.start()
-            return redirect(url_for('create'))
-        elif request.form.get("display"):
-            return redirect(url_for('alarmschedule'))
-        elif request.form.get("clear"):
-            return redirect(url_for('clear'))
-        return render_template('create.html') 
+    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+        if request.method=="POST":
+            if request.form.get("external")=="submit":
+                data = request.form.to_dict(flat=False)
+                start_date = data['start_Date'][0]
+                end_date = data['End_Date'][0]
+                total = len(data)+1
+                t = int(total/3)
+                for i in range(0,t-1):
+                    ti=data['time'+str(i)]
+                    te=data['text'+str(i)]
+                    be=data['select'+str(i)]
+                    time.append(ti)
+                    text.append(te)
+                    bell.append(be)
+                con = sql.connect("real.db") 
+                cur = con.cursor()
+                cur.execute('''CREATE TABLE IF NOT EXISTS SchoolBells(SNO INTEGER PRIMARY KEY AUTOINCREMENT,TIME TEXT NOT NULL,BELLS TEXT NOT NULL,COUNT INT NOT NULL)''')
+                for i in range(0,len(time)):
+                    cur.execute("INSERT INTO SchoolBells(TIME,COUNT,BELLS) VALUES (?,?,?)",(str(time[i][0]),str(text[i][0]),str(bell[i][0]),))
+                con.commit()
+                con.close()
+                #print("database saved")
+                flash("Successfully Saved")
+                act = "create"
+                di[0]= "create"
+                d_s = {}
+                d_l = {}
+                c = {}
+                for i in range(0,t-1):
+                    if bell[i][0] == 'short bell':
+                        d_s[time[i][0]] = i+1
+                        c[time[i][0]]=int(text[i][0])
+                    if bell[i][0] == 'long bell':
+                        d_l[time[i][0]] = i+1
+                thr = Thread(target= new, args=[start_date,end_date,d_s,d_l,c,act])
+                thr.start()
+                return redirect(url_for('create'))
+            elif request.form.get("external")=="display":
+                return redirect(url_for('alarmschedule'))
+            elif request.form.get("external")=="clear":
+                return redirect(url_for('clear'))
+            elif request.form.get("external")=="logout":
+                return redirect(url_for('login'))
+            return render_template('create.html')
+        else:
+            flash("Server Error")
+            return render_template("create.html")
     else:
-        flash("Server Error")
-        return render_template("create.html")
+        abort(404)
 
 @app.route('/clear',methods=["GET","POST"])
 def clear():
-    con = sql.connect("real.db") 
-    cur = con.cursor()
-    cur.execute("DROP TABLE SchoolBells")
-    con.commit()
-    con.close()
-    print("table dropped successfully")
-    flash("Successfully Cleared the Database")
-    return redirect(url_for('create'))
+    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+        try:
+            con = sql.connect("real.db") 
+            cur = con.cursor()
+            cur.execute("DROP TABLE SchoolBells")
+            con.commit()
+            con.close()
+            #print("table dropped successfully")
+            flash("Successfully Cleared the Database")
+            return redirect(url_for('create'))
+        except:
+            return "<h1>Error, Table is already deleted</<h1>"
+    else:
+        abort(404)
 
 @app.route("/alarmschedule",methods=["GET","POST"])
 def alarmschedule():
-    if request.method=="POST":
-        if request.form.get("submit"):
-            return redirect(url_for('create'))
-        elif request.form.get("clear"):
-            return redirect(url_for('clear'))
-        elif request.form.get("display"):
-            return redirect(url_for('alarmschedule'))
+    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+        if request.method=="POST":
+            if request.form.get("external")=="submit":
+                return redirect(url_for('create'))
+            elif request.form.get("external")=="clear":
+                return redirect(url_for('clear'))
+            elif request.form.get("external")=="display":
+                return redirect(url_for('alarmschedule'))
 
-    try:
-        con = sql.connect("real.db")
-        con.row_factory = sql.Row 
-        cur = con.cursor()
-        cur.execute("SELECT SNO,TIME,BELLS,COUNT FROM SchoolBells")
-        rows = cur.fetchall()
-        print(rows)
-        return render_template('scheduledisplayer.html',items= rows)
-    except:
-        return "<h1>Error , Oops Server Error</h1>"
+        try:
+            con = sql.connect("real.db")
+            con.row_factory = sql.Row 
+            cur = con.cursor()
+            cur.execute("SELECT SNO,TIME,BELLS,COUNT FROM SchoolBells")
+            rows = cur.fetchall()
+            return render_template('scheduledisplayer.html',items= rows)
+        except:
+            return "<h1>Error , create a table</h1>"
+    else:
+        abort(404)
 
 
 if __name__=='__main__':
