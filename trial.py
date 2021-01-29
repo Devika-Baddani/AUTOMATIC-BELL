cb5b0a9c103f1f2e3c6d93ac96c7481227738981
Devika-Baddani <75314277+Devika-Baddani@users.noreply.github.com>
2021-01-29 16:48:11 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ from datetime import datetime,timedelta
 import time as t
 from threading import Thread
 import requests
-'''import RPi.GPIO as m
+import RPi.GPIO as m
 
 m.setmode(m.BCM)
 m.setup(17,m.OUT)
@@ -19,7 +19,7 @@ def shortbell():
     m.output(17,1)
     t.sleep(2)
     m.output(17,0)
-    t.sleep(1)'''
+    t.sleep(1)
 app=Flask(__name__)
 app.secret_key = 'PBSAasdertyuiop2020'
 user_pass = {0:"user",1:"password"}
@@ -64,14 +64,14 @@ def total(act,start,end):
     s_k = list(s.keys())
     l = {}
     if(act == "normaldays"):
-        s = {"08:00:00":2,"08:50:00":3,"09:40:00":4,"10:30:00":5,"10:45:00":6,"11:35:00":7,"12:25:00":8,"13:15:00":9,"14:05:00":10,"14:55:00":11,"15:10:00":12,"16:00:00":13,"16:50:00":14,"17:40:00":15}
-        c = {"08:00:00":2,"08:50:00":1,"09:40:00":2,"10:30:00":3,"10:45:00":1,"11:35:00":4,"12:25:00":5,"13:15:00":1,"14:05:00":2,"14:55:00":3,"15:10:00":1,"16:00:00":4,"16:50:00":5,"17:40:00":6}
+        s = {"07:55:00":2,"08:00:00":3,"08:50:00":4,"09:40:00":5,"10:30:00":6,"10:45:00":7,"11:35:00":8,"12:25:00":9,"13:15:00":10,"14:05:00":11,"14:55:00":12,"15:10:00":13,"16:00:00":14,"16:50:00":15,"17:40:00":16}
+        c = {"07:55:00":2,"08:00:00":3,"08:50:00":1,"09:40:00":2,"10:30:00":3,"10:45:00":1,"11:35:00":4,"12:25:00":5,"13:15:00":1,"14:05:00":2,"14:55:00":3,"15:10:00":1,"16:00:00":4,"16:50:00":5,"17:40:00":6}
         s_k = list(s.keys())
         l = {"07:45:00":1}
     elif(act=="internal"):
-        s = {"08:50:00":2,"08:55:00":3,"09:00:00":4,"09:30:00":5,"10:00:00":6,"10:25:00":7,"10:50:00":10,"10:55:00":11,"11:00:00":12,"11:30:00":13,"12:00:00":14,"12:25:00":15,"13:20:00":18,"13:25:00":19,"13:30:00":20,"14:00:00":21,"14:30:00":22,"14:55:00":23,"15:20:00":26,"15:25:00":27,"15:30:00":28,"17:15:00":29,"17:11:00":30,"16:55:00":31}
+        s = {"08:50:00":2,"08:55:00":3,"09:00:00":4,"09:30:00":5,"10:00:00":6,"10:25:00":7,"10:50:00":10,"10:55:00":11,"11:00:00":12,"11:30:00":13,"12:00:00":14,"12:25:00":15,"13:20:00":18,"13:25:00":19,"13:30:00":20,"14:00:00":21,"14:30:00":22,"14:55:00":23,"15:20:00":26,"15:25:00":27,"15:30:00":28,"16:00:00":29,"16:30:00":30,"16:55:00":31}
         l = {"08:45:00":1,"10:30:00":8,"10:45:00":9,"12:30:00":16,"13:15:00":17,"15:00:00":24,"15:15:00":25,"17:00:00":32}
-        c = {"08:50:00":1,"08:55:00":2,"09:00:00":3,"09:30:00":1,"10:00:00":2,"10:25:00":1,"10:50:00":1,"10:55:00":2,"11:00:00":3,"11:30:00":1,"12:00:00":2,"12:25:00":1,"13:20:00":1,"13:25:00":2,"13:30:00":3,"14:00:00":1,"14:30:00":2,"14:55:00":1,"15:20:00":1,"15:25:00":2,"15:30:00":3,"17:15:00":1,"17:11:00":2,"16:55:00":1}
+        c = {"08:50:00":1,"08:55:00":2,"09:00:00":3,"09:30:00":1,"10:00:00":2,"10:25:00":1,"10:50:00":1,"10:55:00":2,"11:00:00":3,"11:30:00":1,"12:00:00":2,"12:25:00":1,"13:20:00":1,"13:25:00":2,"13:30:00":3,"14:00:00":1,"14:30:00":2,"14:55:00":1,"15:20:00":1,"15:25:00":2,"15:30:00":3,"16:00:00":1,"16:30:00":2,"16:55:00":1}
         s_k = list(s.keys())
     elif(act=="semester"):
         s = {"08:50:00":2,"08:55:00":3,"09:00:00":4,"09:30:00":5,"10:00:00":6,"10:30:00":7,"11:00:00":8,"11:30:00":9,"11:45:00":10,"13:50:00":13,"13:55:00":14,"14:00:00":15,"14:30:00":16,"15:00:00":17,"15:30:00":18,"16:00:00":19,"16:30:00":20,"16:45:00":21}
@@ -94,8 +94,8 @@ def total(act,start,end):
                 q = len(dates) if dates.get(date) is None else dates.get(date)
                 #print("q = ",q)
                 print("long bell")
-                #longbell()
-                t.sleep(1)
+                longbell()
+                #t.sleep(1)
             elif m in s and date in dates:
                 p = 1 if s.get(m) is None else s.get(m)
                 #print("p = ", p)
@@ -103,12 +103,10 @@ def total(act,start,end):
                 #print("q = ", q)
                 r = c.get(m)
                 print("The bell should ring", r," times")
-                #thr = Thread(target = send)
-                #thr.start()
                 for i in range(r):
                     print("bell rings")
-                    #shortbell()
-                    t.sleep(1)
+                    shortbell()
+                    #t.sleep(1)
 
 def new(start,end,d_s,d_l,c,act):
     dates = {}
@@ -141,8 +139,8 @@ def new(start,end,d_s,d_l,c,act):
                     q = len(dates) if dates.get(date) is None else dates.get(date)
                     #print("q = ",q)
                     print("long bell")
-                    #longbell()
-                    t.sleep(1)
+                    longbell()
+                    #t.sleep(1)
                 elif m in d_s and date in dates:
                     print(m)
                     p = 1 if d_s.get(m) is None else d_s.get(m)
@@ -153,8 +151,8 @@ def new(start,end,d_s,d_l,c,act):
                     print("The bell should ring", r," times")
                     for i in range(r):
                         print("bell rings")
-                        #shortbell()
-                        t.sleep(1)
+                        shortbell()
+                        #t.sleep(1)
 
 def EMERGENCY():
     print("longbell")
@@ -165,6 +163,8 @@ def EMERGENCY():
     s_d = str(a)[0:10]
     e_d = str(b)[0:10]
     thr = Thread(target = total, args=["stop",s_d,e_d])
+    thr.start()
+    thr = Thread(target = decision, args=["stop",s_d,e_d])
     thr.start()
 
 @app.route('/classify',methods = ["GET","POST"])
@@ -184,24 +184,52 @@ def classify():
         return render_template('selction2.html')
     else:
         abort(404)
+def decision(action,start,end):
+    con = sql.connect("real2.db") 
+    cur = con.cursor()
+    cursor = con.execute("SELECT name from sqlite_master WHERE type = 'table' AND name = 'Decision';")
+    result = cursor.fetchone()
+    cur.execute("DROP TABLE IF EXISTS Decision")
+    cur.execute('''CREATE TABLE IF NOT EXISTS Decision(SNO INTEGER PRIMARY KEY AUTOINCREMENT
+        ,DAY TEXT NOT NULL,Start_date TEXT NOT NULL,End_date TEXT NOT NULL)''')
+    cur.execute("INSERT INTO Decision(DAY,Start_date,End_date) VALUES (?,?,?)",(action,start,end,))
+    con.commit()
+    con.close()
 
 def back_up():
     try:
         di[0] = "decision"
-        con = sql.connect("real1.db")
-        con.row_factory = sql.Row 
+        con = sql.connect("real2.db") 
+        con.row_factory = sql.Row
         cur = con.cursor()
-        cursor = con.execute("SELECT name from sqlite_master WHERE type = 'table' AND name = 'DATE';")
-        result = cursor.fetchone()
-        cursor =con.execute("select DAY,STARTDATE,ENDDATE from DATA")
+        cursor = con.execute("SELECT name from sqlite_master WHERE type = 'table' AND name = 'Decision';")
+        cursor =con.execute("select * from Decision")
         for row in cursor:
-            act = row[0]
-            s_d = row[1]
-            e_d = row[2]
-        thr = Thread(target = total, args = [act,s_d,e_d])
-        thr.start()
+            decision = row[1]
+            start = row[2]
+            end = row[3]
+        #print(decision)
+        if decision == "create":
+            con = sql.connect("real.db") 
+            cur = con.cursor()
+            cursor = con.execute("select * from SchoolBells")
+            d_s = {}
+            d_l = {}
+            c = {}
+            for row in cursor:
+                if row[2] == "short bell":
+                    d_s[row[1]] = row[0]
+                    c[row[1]] = row[3]
+                elif row[2] == "long bell":
+                    d_l[row[1]] = row[0]
+            #print(d_s,d_l,c)
+            thr  =Thread(target = new, args = [start,end,d_s,d_l,c,decision])
+            thr.start()
+        else:
+            thr = Thread(target = total, args = [decision,start,end])
+            thr.start()
     except:
-        print("no values")
+        flash("no values")
 
 @app.route('/exam',methods=["POST","GET"])
 def exam():
@@ -228,6 +256,8 @@ def exam():
         di[0] = "decision"
         thr = Thread(target = total, args = [act,s_d,e_d])
         thr.start()
+        th = Thread(target = decision, args = [act,s_d,e_d])
+        th.start()
         return render_template('selection1.html')
     else:
         abort(404)
@@ -276,6 +306,9 @@ def create():
                         d_l[time[i][0]] = i+1
                 thr = Thread(target= new, args=[start_date,end_date,d_s,d_l,c,act])
                 thr.start()
+                th = Thread(target = decision, args = [act,start_date,end_date])
+                th.start()
+
                 return redirect(url_for('create'))
             elif request.form.get("external")=="display":
                 return redirect(url_for('alarmschedule'))
