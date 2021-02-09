d340acf14d30da8a2f0ab7a7a35e6a4386d0a91e
Devika-Baddani <75314277+Devika-Baddani@users.noreply.github.com>
2021-02-09 13:17:46 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -36,7 +36,7 @@ def login():
 def loginvalid():
     username = request.form['username']
     password = request.form['password']
-    if username=="admin" and password=="asdf":
+    if username=="****" and password=="****":
         user_pass[0] = username
         user_pass[1] = password
         return render_template('selection2.html')
@@ -44,117 +44,6 @@ def loginvalid():
         return render_template('login.html')
 
 di = {0:"decision"}
-def total(act,start,end):
-    a = datetime.strptime(start,"%Y-%m-%d")
-    b = datetime.strptime(end,"%Y-%m-%d")
-    delta = b-a
-    print(act)
-    #print("Total number of days: ",delta.days)
-    print("Scheduled for the following days: ")
-    di[0] = act
-    dates = {}
-    for i in range(0,delta.days):
-        day = a + timedelta(days=i)
-        if day.weekday() != 6:
-            dates[str(day)[0:10]] = i+1
-            print(str(day)[0:10])
-    #print("Total dates dictionary:",dates)
-    q = 0
-    s = {}
-    c = {}
-    s_k = list(s.keys())
-    l = {}
-    if(act == "normaldays"):
-        s = {"07:55:00":2,"08:00:00":3,"08:50:00":4,"09:40:00":5,"10:30:00":6,"10:45:00":7,"11:35:00":8,"12:25:00":9,"13:15:00":10,"14:05:00":11,"14:55:00":12,"15:10:00":13,"16:00:00":14,"16:50:00":15,"17:40:00":16}
-        c = {"07:55:00":2,"08:00:00":3,"08:50:00":1,"09:40:00":2,"10:30:00":3,"10:45:00":1,"11:35:00":4,"12:25:00":5,"13:15:00":1,"14:05:00":2,"14:55:00":3,"15:10:00":1,"16:00:00":4,"16:50:00":5,"17:40:00":6}
-        s_k = list(s.keys())
-        l = {"07:45:00":1}
-    elif(act=="internal"):
-        s = {"08:50:00":2,"08:55:00":3,"09:00:00":4,"09:30:00":5,"10:00:00":6,"10:25:00":7,"10:50:00":10,"10:55:00":11,"11:00:00":12,"11:30:00":13,"12:00:00":14,"12:25:00":15,"13:20:00":18,"13:25:00":19,"13:30:00":20,"14:00:00":21,"14:30:00":22,"14:55:00":23,"15:20:00":26,"15:25:00":27,"15:30:00":28,"16:00:00":29,"16:30:00":30,"16:55:00":31}
-        l = {"08:45:00":1,"10:30:00":8,"10:45:00":9,"12:30:00":16,"13:15:00":17,"15:00:00":24,"15:15:00":25,"17:00:00":32}
-        c = {"08:50:00":1,"08:55:00":2,"09:00:00":3,"09:30:00":1,"10:00:00":2,"10:25:00":1,"10:50:00":1,"10:55:00":2,"11:00:00":3,"11:30:00":1,"12:00:00":2,"12:25:00":1,"13:20:00":1,"13:25:00":2,"13:30:00":3,"14:00:00":1,"14:30:00":2,"14:55:00":1,"15:20:00":1,"15:25:00":2,"15:30:00":3,"16:00:00":1,"16:30:00":2,"16:55:00":1}
-        s_k = list(s.keys())
-    elif(act=="semester"):
-        s = {"08:50:00":2,"08:55:00":3,"09:00:00":4,"09:30:00":5,"10:00:00":6,"10:30:00":7,"11:00:00":8,"11:30:00":9,"11:45:00":10,"13:50:00":13,"13:55:00":14,"14:00:00":15,"14:30:00":16,"15:00:00":17,"15:30:00":18,"16:00:00":19,"16:30:00":20,"16:45:00":21}
-        l = {"08:45:00":1,"12:00:00":11,"13:45:00":12,"17:00:00":22}
-        c = {"08:50:00":1,"08:55:00":2,"09:00:00":3,"09:30:00":1,"10:00:00":2,"10:30:00":3,"11:00:00":4,"11:30:00":5,"11:45:00":1,"13:50:00":1,"13:55:00":2,"14:00:00":3,"14:30:00":1,"15:00:00":2,"15:30:00":3,"16:00:00":4,"16:30:00":5,"16:45:00":1}
-        s_k = list(s.keys())
-    elif(act=="stop"):
-        s = {}
-        l = {}
-        c = {}
-        s_k = list(s.keys())
-    while q<len(dates) and act== di.get(0):
-        p = 0
-        while p < len(s)+len(l) and act == di.get(0):
-            date = t.strftime("%Y-%m-%d")
-            m = t.strftime("%H:%M:%S")
-            if m in l and date in dates:
-                p = 1 if l.get(m) is None else l.get(m)
-                #print("p = ",p)
-                q = len(dates) if dates.get(date) is None else dates.get(date)
-                #print("q = ",q)
-                print("long bell")
-                longbell()
-                #t.sleep(1)
-            elif m in s and date in dates:
-                p = 1 if s.get(m) is None else s.get(m)
-                #print("p = ", p)
-                q = len(dates) if dates.get(date) is None else dates.get(date)
-                #print("q = ", q)
-                r = c.get(m)
-                print("The bell should ring", r," times")
-                for i in range(r):
-                    print("bell rings")
-                    shortbell()
-                    #t.sleep(1)
-
-def new(start,end,d_s,d_l,c,act):
-    dates = {}
-    if len(start)!=0 and len(end)!=0:
-        a = datetime.strptime(start,"%Y-%m-%d")
-        b = datetime.strptime(end,"%Y-%m-%d")
-        delta = b-a
-        #print(act)
-        #print("Total number of days: ",delta.days)
-        print("The following dates are scheduled: ")
-        di[0] = act
-        #print(di)
-        dates = {}
-        for i in range(0,delta.days):
-            day = a + timedelta(days=i)
-            dates[str(day)[0:10]] = i+1
-            print(str(day)[0:10])
-        #print("Total dates dictionary:",dates)
-        q = 0
-        s_k = list(d_s.keys())
-        while q< len(dates) and act== di.get(0):
-            p = 0 
-            while p < len(d_s)+len(d_l) and act==di.get(0):
-                date = t.strftime("%Y-%m-%d")
-                m = t.strftime("%H:%M:%S")
-                if m in d_l and date in dates:
-                    print(m)
-                    p = 1 if d_l.get(m) is None else d_l.get(m)
-                    #print("p = ",p)
-                    q = len(dates) if dates.get(date) is None else dates.get(date)
-                    #print("q = ",q)
-                    print("long bell")
-                    longbell()
-                    #t.sleep(1)
-                elif m in d_s and date in dates:
-                    print(m)
-                    p = 1 if d_s.get(m) is None else d_s.get(m)
-                    #print("p = ",p)
-                    q = len(dates) if dates.get(date) is None else dates.get(date)
-                    #print("q = ",q)
-                    r = c.get(m)
-                    print("The bell should ring", r," times")
-                    for i in range(r):
-                        print("bell rings")
-                        shortbell()
-                        #t.sleep(1)
-
 def EMERGENCY():
     print("longbell")
     longbell()
@@ -234,7 +123,7 @@ def back_up():
 
 @app.route('/exam',methods=["POST","GET"])
 def exam():
-    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+    if user_pass[0]=="****" and user_pass[1]=="****":
         a = request.form
         con = sql.connect("real1.db") 
         cur = con.cursor()
@@ -269,7 +158,7 @@ def create():
     time = []
     text = []
     bell = []
-    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+    if user_pass[0]=="****" and user_pass[1]=="****":
         if request.method=="POST":
             if request.form.get("external")=="submit":
                 data = request.form.to_dict(flat=False)
@@ -326,7 +215,7 @@ def create():
 
 @app.route('/clear',methods=["GET","POST"])
 def clear():
-    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+    if user_pass[0]=="****" and user_pass[1]=="****":
         try:
             con = sql.connect("real.db") 
             cur = con.cursor()
@@ -343,7 +232,7 @@ def clear():
 
 @app.route("/alarmschedule",methods=["GET","POST"])
 def alarmschedule():
-    if user_pass[0]=="admin" and user_pass[1]=="asdf":
+    if user_pass[0]=="****" and user_pass[1]=="****":
         if request.method=="POST":
             if request.form.get("external")=="submit":
                 return redirect(url_for('create'))
